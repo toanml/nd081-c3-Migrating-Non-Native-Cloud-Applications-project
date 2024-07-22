156fe751cfdcf60f78c789eb2f5d67ed3d831f29
Toan Mai <toan.lemai@coxautoinc.com>
2024-07-22 14:21:55 +0700

    Remove keys

--- function/settings.py
+++ function/settings.py
@@ -1,8 +1,9 @@
 POSTGRES_URL="postgres-server-mlt.postgres.database.azure.com"  #TODO: Update value
 POSTGRES_USER="azureuser@postgres-server-mlt" #TODO: Update value
-POSTGRES_PW="PaSSw0rD"   #TODO: Update value
+POSTGRES_PW=""   #TODO: Update value
+POSTGRES_DB="techconfdb"   #TODO: Update value
 SERVICE_BUS_CONNECTION_STRING ='' #TODO: Update value
 SERVICE_BUS_QUEUE_NAME ='notificationqueue'
 ADMIN_EMAIL_ADDRESS = 'mailetoan@gmail.com'
-GMAIL_USER = 'mailetoan'
-GMAIL_PASSWORD = 'LeToan@1982'
+GMAIL_USER = ''
+GMAIL_PASSWORD = ''
