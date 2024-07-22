e977304fa6c122eb1aba586bb58a2839cf81c085
Toan Mai <toan.lemai@coxautoinc.com>
2024-07-22 14:10:46 +0700

    Complete course

--- function/settings.py
+++ function/settings.py
@@ -1,7 +1,6 @@
 POSTGRES_URL="postgres-server-mlt.postgres.database.azure.com"  #TODO: Update value
 POSTGRES_USER="azureuser@postgres-server-mlt" #TODO: Update value
 POSTGRES_PW="PaSSw0rD"   #TODO: Update value
-POSTGRES_DB="techconfdb"   #TODO: Update value
 SERVICE_BUS_CONNECTION_STRING ='' #TODO: Update value
 SERVICE_BUS_QUEUE_NAME ='notificationqueue'
 ADMIN_EMAIL_ADDRESS = 'mailetoan@gmail.com'
